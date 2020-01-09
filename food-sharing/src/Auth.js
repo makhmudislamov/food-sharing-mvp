9475727cfbbe09658027cebd385325710730e125
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-01-08 21:24:52 -0800

    login page setup

--- food-sharing/src/Auth.js
+++ food-sharing/src/Auth.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
+import LoginCard from './components/auth/loginCard';
 
 
 class Auth extends Component {
     // state = {  }
     render() { 
-        return ( 
-            <Navbar />
-            <Footer />
-         );
+        return (
+            <React.Fragment>
+                <Navbar />
+                <LoginCard />
+                <Footer />
+            </React.Fragment>
+        );
     }
 }
  
