2ccc83892e4d993396f2cb82d1115d6ff96e9f52
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-16 18:40:12 -0700

    slight cleanup

--- food-sharing/FRONT-END/src/App.js
+++ food-sharing/FRONT-END/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
+import { Router, Switch, Route  } from "react-router-dom";
 import NavigationBar from './components/navbar';
 import Footer from './components/footer';
 import Body from './components/homePageBody';
-import { BrowserRouter as Router, Switch, Route  } from "react-router-dom";
 import Auth from "./components/Auth";
 import SignUp from "./components/SignUp";
 import Dashboard from "./components/Dashboard";
@@ -11,18 +11,18 @@ class App extends Component {
   // state = {  }
   render() { 
     return (
-        <Router>
-            <Switch>
-                <React.Fragment>
-                    <NavigationBar />
-                    <Route path="/" exact component={Body} />
-                    <Route path="/login" component={Auth} />
-                    <Route path="/signup" component={SignUp} />
-                    <Route path="/dashboard" component={Dashboard} />
-                    <Footer />
-                </React.Fragment>
-            </Switch>
-        </Router>
+        <React.Fragment>
+            <NavigationBar />
+              <main className="conrainter">
+                <Switch>
+                  <Route path="/" exact component={Body} />
+                  <Route path="/login" component={Auth} />
+                  <Route path="/signup" component={SignUp} />
+                  <Route path="/dashboard" component={Dashboard} />
+                </Switch>
+              </main>
+          <Footer />
+        </React.Fragment>
     );
   }
 }
--- food-sharing/FRONT-END/src/index.js
+++ food-sharing/FRONT-END/src/index.js
@@ -1,17 +1,15 @@
-// import "./custom.scss";
-// import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
-// import Auth from './Auth';
-// import * as serviceWorker from './serviceWorker';
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "./css/main.css";
 
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,    
+        document.getElementById('root')
+    
+    );
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// ReactDOM.render(<Auth />, document.getElementById('main'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
