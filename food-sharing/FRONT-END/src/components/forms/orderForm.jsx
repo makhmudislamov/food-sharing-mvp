9c32e3405bc8aeff6545847c675f806140bf4c9b
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-20 15:28:15 -0700

    created new form to post new food

--- food-sharing/FRONT-END/src/components/forms/orderForm.jsx
+++ food-sharing/FRONT-END/src/components/forms/orderForm.jsx
@@ -4,10 +4,58 @@ import Joi from "joi-browser";
 import FormMethods from "./common/formMethods";
 
 class OrderForm extends FormMethods {
-    state = {  }
-    render() { 
-        return ( 
-            <h1>Order will be here</h1>
+    state = {
+        data: {
+            status: "Pickup in Progress", // this should be maybe array with all possible statuses
+            foodName: "",
+            amount: ""
+        },
+        errors: {}
+    };
+
+    schema = {
+        status: Joi.string()
+            .required()
+            .label("Pickup Status"),
+        foodName: Joi.string()
+            .required()
+            .label("Food Name"),
+        amount: Joi.string()
+            .required()
+            .label("Amount")
+    };
+
+    doSubmit = () => {
+        // call the server
+        console.log("posted new food order");
+    };
+    render() {
+        return (
+            <Card
+                style={{
+                    width: "30rem",
+                    marginLeft: "200px",
+                    marginTop: "150px"
+                }}
+            >
+                <Card.Body>
+                    <Card.Title>Create new order</Card.Title>
+                    <Form onSubmit={this.handleSubmit}>
+                        {this.renderInput(
+                            "status",
+                            "Status",
+                            "Pickup in Progress"
+                        )}
+                        {this.renderInput(
+                            "foodName",
+                            "Food Name",
+                            "Chicken Burrito"
+                        )}
+                        {this.renderInput("amount", "Amount", "4")}
+                        {this.renderButton("Submit")}
+                    </Form>
+                </Card.Body>
+            </Card>
         );
     }
 }
