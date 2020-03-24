40fd2cfe51cf604ff0cd4d8fdccd5f9c9c4cbed0
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-24 09:20:52 -0700

    fixed bidirectional dependencies

--- food-sharing/FRONT-END/src/components/services/httpService.js
+++ food-sharing/FRONT-END/src/components/services/httpService.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 import logger from  "./logService";
-import auth from './authService'
-axios.defaults.headers.common['x-auth-token'] = auth.getJwt();
-
 axios.interceptors.response.use(null, error => {
     const expectedError =
         error.response &&
@@ -16,10 +13,15 @@ axios.interceptors.response.use(null, error => {
     return Promise.reject(error);
 });
 
+function setJwt(jwt) {
+    axios.defaults.headers.common["x-auth-token"] = jwt;
+}
+
 
 export default {
     get: axios.get,
     post: axios.post,
     put: axios.put,
-    delete: axios.delete
+    delete: axios.delete,
+    setJwt
 }
