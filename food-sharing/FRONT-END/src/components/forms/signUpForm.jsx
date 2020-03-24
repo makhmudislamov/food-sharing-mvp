830e240828dcbfd8d0cc959913c43439ebed9430
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-03-24 08:07:02 -0700

    loggin user upon signup

--- food-sharing/FRONT-END/src/components/forms/signUpForm.jsx
+++ food-sharing/FRONT-END/src/components/forms/signUpForm.jsx
@@ -47,7 +47,9 @@ class SignUpForm extends FormMethods {
     doSubmit = async () => {
         try {
             // call the server
-            await userService.register(this.state.data)        
+            const response = await userService.register(this.state.data);
+            localStorage.setItem("token", response.headers['x-auth-token']);
+            this.props.history.push("/orders")
         } catch (ex) {
             if (ex.response && ex.response.status === 400) {
                 const errors = {...this.state.errors};
